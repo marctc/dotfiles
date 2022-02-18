48c318bc80ec2a1b0752d09390a32d1993875efb
Marc Tuduri <marc.tuduri@soundcloud.com>
2022-02-18 15:57:22 +0100

    last changes

--- i3wm/status.py
+++ i3wm/status.py
@@ -8,10 +8,19 @@ status = Status(standalone=True)
 status.register("clock",
                 format=" %d/%m/%Y %H:%M",)
 
+status.register("battery",
+    format="{percentage:.2f}% | {remaining:%E%hh:%Mm}",
+    alert=False,
+    alert_percentage=5,
+    status={
+        "DIS": "↓",
+        "CHR": "↑",
+        "FULL": "=",
+    },)
 
-status.register("network",
-                interface="eno1",
-                format_up="{v4cidr}",)
+# status.register("network",
+#                 interface="veth40851ef",
+#                 format_up="{v4cidr}",)
 
 status.register("disk",
                 path="/",
